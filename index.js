e921ba13786b4cedcd95afaee3ab7019ab69e2f1
avijitsaha118 <sahaavijit908@gmail.com>
2023-04-01 22:38:22 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+//midleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jauckc9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const itemCollection = client.db("wareManage").collection("items");
+
+    //ITEMS API
+    app.get("/item", async (req, res) => {
+      const query = {};
+      const cursor = itemCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+
+    app.get('/item/:id', async (req, res) => {
+        const id = req.params.id;
+
+        const query = { _id: ObjectId(id) };
+        const item = await itemCollection.findOne(query);
+        res.send(item);
+    });
+
+    //POST
+    app.post('/item', async (req, res) => {
+        const newItem = req.body;
+        const result = await itemCollection.insertOne(newItem);
+        res.send(result);
+    });
+
+    //DELETE
+
+    app.delete('/item/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await itemCollection.deleteOne(query);
+        res.send(result);
+    });
+
+    //Updated Quantity
+
+    app.put('/item/:id', async (req, res) => {
+        const id = req.params.id;
+        const updatedItem = req.body;
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updatedDoc = {
+            $set: {
+                quantity: updatedItem.quantity
+            }
+        };
+        const result = await itemCollection.updateOne(filter, updatedDoc, options);
+        res.send(result);
+
+    });
+
+    //myitem api
+
+    app.get('/myitem', async (req, res) => {
+
+        const email = req.query.email;
+
+        const query = { email: email };
+        const cursor = itemCollection.find(query);
+        // const cursor = myItemCollection.find(query);
+        const myItems = await cursor.toArray();
+        res.send(myItems);
+
+    });
+
+    app.post('/myitem', async (req, res) => {
+        const item = req.body;
+        const result = await itemCollection.insertOne(item);
+        // const result = await myItemCollection.insertOne(item);
+        res.send(result);
+    })
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running warehouse management server");
+});
+
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
